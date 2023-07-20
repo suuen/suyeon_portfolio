b5c6d7bb42ca60834b236c4945c423d7124ae813
suyeno <realsu_yeonini418@naver.com>
2023-07-20 19:52:20 +0900

    firstcode

---
+++ app/src/main/java/com/example/myapplication/first.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+fun main() {
+
+    print("첫 번째 숫자를 입력하세요: ")
+    val num1 = readLine()!!.toDouble()
+
+    print("두 번째 숫자를 입력하세요: ")
+    val num2 = readLine()!!.toDouble()
+
+    print("사칙연산을 입력하세요: ")
+    val operator = readLine()
+
+    if (operator == "+") {
+        val result = num1 + num2
+        println("덧셈 결과: $result")
+    } else if (operator == "-") {
+        val result = num1 - num2
+        println("뺄셈 결과: $result")
+    } else if (operator == "/")
+    {
+        val result = num1 / num2
+        println("덧셈 결과: $result")
+    } else if (operator == "*") {
+        val result = num1 * num2
+        println("덧셈 결과: $result")
+    } else {
+        println("잘못된 연산자 입력입니다.")
+    }
+}
