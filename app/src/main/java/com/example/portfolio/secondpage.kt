ed00eeab53f54a42f9874896c290088a1bc3afa9
suyeno <realsu_yeonini418@naver.com>
2023-08-31 21:42:30 +0900

    [FEAT] : 상세 페이지 뒤로가기 버튼 추가/기능 구현

--- app/src/main/java/com/example/portfolio/secondpage.kt
+++ app/src/main/java/com/example/portfolio/secondpage.kt
@@ -1,9 +1,11 @@
 package com.example.portfolio
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.portfolio.databinding.DetailpageBinding
 import java.text.DecimalFormat
+
 
 class secondpage : AppCompatActivity() {
 
@@ -27,7 +29,16 @@ class secondpage : AppCompatActivity() {
             binding.priceB.text = price
             binding.iconB.setImageResource(receivedData.aIcon)
 
+            val backpageButton = findViewById<ImageView>(R.id.back_page)
+
+            backpageButton.setOnClickListener{
+
+                super.onBackPressed() // 뒤로가기 버튼
+            }
+
 
         }
+
+
     }
 }
