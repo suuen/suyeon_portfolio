6b62471dce0b9c95a57adbc0747e22acbafeed2a
suyeno <realsu_yeonini418@naver.com>
2023-09-19 15:47:04 +0900

    [FEAT] 생명주기 확인 LOG

--- app/src/main/java/com/example/example/MainActivity.kt
+++ app/src/main/java/com/example/example/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.example.example
 
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 
-class MainActivity(){
 
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        showToast("Hello World")
+
+        showToast("Custom Duration Toast", Toast.LENGTH_LONG)
+    }
+
+    fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, duration).show()
+    }
 }
+
+
