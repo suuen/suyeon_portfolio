9bcc32b9ad1a5fb747029877546226101a05fbda
suyeno <realsu_yeonini418@naver.com>
2023-09-22 00:16:15 +0900

    [UI] 전체 UI 작성

--- app/src/main/java/com/example/kakaobank_a/MainActivity.kt
+++ app/src/main/java/com/example/kakaobank_a/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.kakaobank_a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kakaobank_a.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.run {
+            btn.setOnClickListener{
+
+            }// 프레그먼트 1로 이동
+            btn2.setOnClickListener{
+
+            }// 프레그먼트 2로 이동
+        }
+
+
     }
 }
